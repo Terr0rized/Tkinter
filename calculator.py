c69a029a38dfd9ca63bd484131074d7f6382f96c
Muzafar Yusup <muzafar.yusup00@gmail.com>
2020-04-08 21:08:55 +0600

    first commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+
+firs_number = int(input("Enter number: "))
+second_number = int(input('Enter number: '))
+operation = input("Enter operation: ")
+if operation == '+':
+    print(firs_number + second_number)
+elif operation == '-':
+    print(firs_number - second_number)
+elif operation == '/':
+    print(firs_number // second_number)
+elif operation == '*':
+    print(firs_number * second_number)
